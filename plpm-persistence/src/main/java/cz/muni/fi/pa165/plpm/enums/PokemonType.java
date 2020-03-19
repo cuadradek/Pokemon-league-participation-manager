1369df877140fb999bcea3b19e2072522d09fa64
Veronika Loukotová <xloukot@fi.muni.cz>
2020-03-19 17:04:27 +0100

    Add Pokemon enity, PokemonType enum

---
+++ plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/enums/PokemonType.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pa165.plpm.enums;
+
+/**
+ * Represents types of pokemons.
+ *
+ * @author Veronika Loukotová
+ */
+public enum PokemonType {
+    NORMAL,
+    FIGHTING,
+    PLYING,
+    POISON,
+    GROUND,
+    ROCK,
+    BUG,
+    GHOST,
+    STEEL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    PSYCHIC,
+    ICE,
+    DRAGON,
+    FAIRY,
+    DARK,
+}
