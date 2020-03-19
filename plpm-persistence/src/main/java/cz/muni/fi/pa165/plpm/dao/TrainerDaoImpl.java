e4cda89f5e337707e7325a1c8366f40b68e49690
cuadradek <xcerhak@fi.muni.cz>
2020-03-19 18:39:55 +0100

    Trainer entity created, trainerDAO implemented

--- plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/dao/TrainerDaoImpl.java
+++ plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/dao/TrainerDaoImpl.java
@@ -1,4 +1,63 @@
 package cz.muni.fi.pa165.plpm.dao;
 
-public class TrainerDaoImpl {
+
+import cz.muni.fi.pa165.plpm.entity.Trainer;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ * Trainer DAO implementation.
+ *
+ * @author Radoslav Cerhak
+ */
+@Repository
+@Transactional
+public class TrainerDaoImpl implements TrainerDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void createTrainer(Trainer trainer) {
+        em.persist(trainer);
+    }
+
+    @Override
+    public void updateTrainer(Trainer trainer) {
+        em.merge(trainer);
+    }
+
+    @Override
+    public void deleteTrainer(Trainer trainer) {
+        em.remove(trainer);
+    }
+
+    @Override
+    public Trainer findTrainerById(Long id) {
+        return em.find(Trainer.class, id);
+    }
+
+    @Override
+    public List<Trainer> findTrainerByFirstName(String firstName) {
+        TypedQuery<Trainer> query = em.createQuery("select t from Trainer t where lower(t.firstName) like :name", Trainer.class);
+        query.setParameter("name", "%" + firstName.toLowerCase() + "%");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Trainer> findTrainerByLastName(String lastName) {
+        TypedQuery<Trainer> query = em.createQuery("select t from Trainer t where lower(t.lastName) like :name", Trainer.class);
+        query.setParameter("name", "%" + lastName.toLowerCase() + "%");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Trainer> findAllTrainers() {
+        return em.createQuery("select t from Trainer t", Trainer.class).getResultList();
+    }
 }
--- plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/entity/Trainer.java
+++ plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/entity/Trainer.java
@@ -1,4 +1,86 @@
 package cz.muni.fi.pa165.plpm.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Class representing entity trainer.
+ *
+ * @author Radoslav Cerhak
+ */
+@Entity
 public class Trainer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    public Trainer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date dateOfBirth) {
+        this.birthDate = dateOfBirth;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Trainer)) return false;
+        Trainer trainer = (Trainer) o;
+        return getFirstName().equals(trainer.getFirstName()) &&
+                getLastName().equals(trainer.getLastName()) &&
+                getBirthDate().equals(trainer.getBirthDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getBirthDate());
+    }
 }
