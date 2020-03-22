ae533d8ee194a9bcd59740f8895078ad22ef7166
Jakub Doczy <x423199@mail.muni.cz>
2020-03-22 10:10:06 +0100

    Added @Repository annotation

--- plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/dao/BadgeDaoImpl.java
+++ plpm-persistence/src/main/java/cz/muni/fi/pa165/plpm/dao/BadgeDaoImpl.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165.plpm.dao;
 
 import cz.muni.fi.pa165.plpm.entity.Badge;
 import cz.muni.fi.pa165.plpm.entity.Trainer;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,6 +14,7 @@ import java.util.Collection;
  *
  * @author Jakub Doczy
  */
+@Repository
 public class BadgeDaoImpl implements BadgeDao {
     @PersistenceContext
     private EntityManager em;
