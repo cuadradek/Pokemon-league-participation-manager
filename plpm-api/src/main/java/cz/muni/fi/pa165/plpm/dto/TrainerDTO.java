8b87e4884cd48e9ba2ced098c023f6c6eb473d15
cuadradek <xcerhak@fi.muni.cz>
2020-04-17 17:22:08 +0200

    new modules added

---
+++ plpm-api/src/main/java/cz/muni/fi/pa165/plpm/dto/TrainerDTO.java
@@ -0,0 +1,4 @@
+package cz.muni.fi.pa165.plpm.dto;
+
+public class TrainerDTO {
+}
---
+++ plpm-service/src/main/java/cz/muni/fi/pa165/plpm/service/facade/TrainerFacadeImpl.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.plpm.service.facade;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TrainerFacadeImpl implements TrainerFacade {
+}
