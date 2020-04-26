2f2a433ee86f4a2981548158c44595daf8d47eb3
Veronika Loukotová <xloukot@fi.muni.cz>
2020-04-26 18:27:04 +0200

    Introduce PlpmServiceException in PokemonService, edit implementation to pass tests

--- plpm-service/src/main/java/cz/muni/fi/pa165/plpm/service/PokemonService.java
+++ plpm-service/src/main/java/cz/muni/fi/pa165/plpm/service/PokemonService.java
@@ -3,14 +3,15 @@ package cz.muni.fi.pa165.plpm.service;
 import java.util.List;
 import cz.muni.fi.pa165.plpm.entity.Pokemon;
 import cz.muni.fi.pa165.plpm.entity.Trainer;
+import cz.muni.fi.pa165.plpm.exceptions.PlpmServiceException;
 
 /**
  * @author: Veronika Loukotova
  */
 public interface PokemonService {
-    Pokemon createPokemon(Pokemon pokemon);
+    Pokemon createPokemon(Pokemon pokemon) throws PlpmServiceException;
 
-    void updatePokemonInfo(Pokemon pokemon);
+    void updatePokemonInfo(Pokemon pokemon) throws PlpmServiceException;
 
     void deletePokemon(Pokemon pokemon);
 
