41f91226693749eaac5188d2eb8e95e48433209e
Jakub Doczy <x423199@fi.muni.cz>
2020-04-26 12:04:40 +0200

    Removed constructor from TrainerFacade - no longer needed

--- plpm-service/src/main/java/cz/muni/fi/pa165/plpm/service/facade/TrainerFacadeImpl.java
+++ plpm-service/src/main/java/cz/muni/fi/pa165/plpm/service/facade/TrainerFacadeImpl.java
@@ -29,12 +29,6 @@ public class TrainerFacadeImpl implements TrainerFacade {
     @Autowired
     private BeanMappingService beanMappingService;
 
-    @Autowired
-    public TrainerFacadeImpl(TrainerService trainerService, BeanMappingService beanMappingService) {
-        this.trainerService = trainerService;
-        this.beanMappingService = beanMappingService;
-    }
-
     @Override
     public Long createTrainer(TrainerCreateDTO trainerCreateDTO) throws PlpmServiceException {
         return trainerService.createTrainer(beanMappingService
