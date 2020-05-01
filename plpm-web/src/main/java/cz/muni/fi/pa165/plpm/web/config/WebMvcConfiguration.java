843087dd9c4ed83da41b2860777755f2d361bb54
cuadradek <xcerhak@fi.muni.cz>
2020-05-01 14:43:39 +0200

    module plpm-web created and configured

---
+++ plpm-web/src/main/java/cz/muni/fi/pa165/plpm/web/config/WebMvcConfiguration.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.pa165.plpm.web.config;
+
+import cz.muni.fi.pa165.plpm.sampledata.SampleDataConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.validation.Validator;
+
+@EnableWebMvc
+@Configuration
+@Import(SampleDataConfiguration.class)
+@ComponentScan(basePackages = "cz.muni.fi.pa165.plpm.web.controllers")
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
---
+++ plpm-web/src/main/java/cz/muni/fi/pa165/plpm/web/controllers/TrainerController.java
@@ -0,0 +1,4 @@
+package cz.muni.fi.pa165.plpm.web.controllers;
+
+public class TrainerController {
+}
