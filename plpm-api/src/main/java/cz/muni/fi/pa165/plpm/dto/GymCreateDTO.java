f77d336ed77e9963f05fad01e2a2e56e97cfb49c
Karolina Kolouchova <karolina.kolouchova@gmail.com>
2020-06-06 22:22:27 +0200

    implemented equals and hashcode in GymDTO and GymCreateDTO

--- plpm-api/src/main/java/cz/muni/fi/pa165/plpm/dto/GymCreateDTO.java
+++ plpm-api/src/main/java/cz/muni/fi/pa165/plpm/dto/GymCreateDTO.java
@@ -4,6 +4,7 @@ import cz.muni.fi.pa165.plpm.dto.TrainerDTO;
 import cz.muni.fi.pa165.plpm.enums.PokemonType;
 
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * @author Karolína Kolouchová
@@ -41,5 +42,20 @@ public class GymCreateDTO {
 
     public void setType(PokemonType type) {
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof GymCreateDTO)) return false;
+        GymCreateDTO gym = (GymCreateDTO) o;
+        return Objects.equals(trainerId, gym.getTrainerId()) && Objects.equals(city, gym.getCity())
+                && type == gym.getType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(trainerId, city, type);
     }
 }
--- plpm-api/src/main/java/cz/muni/fi/pa165/plpm/dto/GymDTO.java
+++ plpm-api/src/main/java/cz/muni/fi/pa165/plpm/dto/GymDTO.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pa165.plpm.dto;
 
 import cz.muni.fi.pa165.plpm.enums.PokemonType;
 
+import java.util.Objects;
+
 /**
  * @author Karolína Kolouchová
  */
@@ -45,5 +47,20 @@ public class GymDTO {
 
     public void setType(PokemonType type) {
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof GymDTO)) return false;
+        GymDTO gym = (GymDTO) o;
+        return Objects.equals(leader, gym.getLeader()) && Objects.equals(city, gym.getCity())
+                && type == gym.getType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(leader, city, type);
     }
 }
